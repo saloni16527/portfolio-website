f42909c5b3717da2671fdb47c14bf35d0e96c274
saloni16527 <salonizoldyck@gmail.com>
2026-07-09 13:41:18 +0530

    change the ui

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,37 +1,73 @@
 import "./style.css"
 import Navbar from "./Navbar";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import cafe from "../assets/cafe.png";
+import portfolio from "../assets/portfolio.png";
+import game from "../assets/game.png";
 
 function Projects() {
    return <>
   <h1 id="proj">Projects</h1>
   <div className="wave-container">
   <div className="card">
-   <div class="pro">
+    <div className="pro">
         <div>
-          <iframe src=" https://saloni16527.github.io/Web-page/" className="img"></iframe>  
-        </div>
-        <h3 className="text">THE GREEN CAFE</h3>
-        <p className="info">Responsive Web Page of Green Cafe build using HTML and CSS</p>
-        <a className="git">Github link</a>
+            <img src={cafe} className="img"/> 
+        </div> 
+        <h3 className="text">☕ THE GREEN CAFE</h3>
+        <p className="info">A modern and responsive Green cafe webpage with an elegant design and a clean user experience interface.</p>
+        <aside>
+        <FaHtml5 className="logo"/>
+        <FaCss3Alt className="logo"/>
+        </aside>
+        <button className="demo">
+          Live Demo <BsBoxArrowUpRight />
+        </button>
+         <button className="gthb">
+          <FaGithub /> GitHub
+        </button>
     </div>
    
-    <div class="pro">
+    <div className="pro">
         <div>
-           <iframe src=" https://saloni16527.github.io/stick-hero-game/" className="img"></iframe> 
-        </div>
-        <h3 className="text">HERO GAME</h3>
-        <p className="info">Responsive Stick Hero Game build using Javascript and React.js</p>
-        <a className="git">Github link</a>
+            <img src={game} className="img"/> 
+        </div> 
+        <h3 className="text">🎮 STICK HERO GAME</h3>
+        <p className="info">A responsive browser game built with Gemini Canvas,featuring smooth animations and engaging gameplay.</p>
+        <aside>
+        < IoLogoJavascript className="logo"/>
+        <FaReact className="logo"/>
+        </aside>
+        <button className="demo">
+          Live Demo <BsBoxArrowUpRight />
+        </button>
+         <button className="gthb">
+          <FaGithub className="cat"/> GitHub
+        </button>
     </div>
 
-    <div class="pro">
+    <div className="pro">
         <div>
-         <iframe src="https://saloni16527.github.io/portfolio-website/" className="img"></iframe>
-        </div>
-        <h3 className="text">PORTFOLIO</h3>
-        <p className="info">Responsive Portfolio Website build Using React</p>
-        <a className="git">Github link</a>
-        </div>
+            <img src={portfolio} className="img"/> 
+        </div> 
+        <h3 className="text">💻 PORTFOLIO</h3>
+        <p className="info">A responsive personal portfolio website showcasing my projects, skills, and frontend development journey.</p>
+        <FaHtml5 className="logo"/>
+        <FaCss3Alt className="logo"/>
+        < IoLogoJavascript className="logo"/>
+        <FaReact className="logo"/>
+        <button className="demo">
+          Live Demo <BsBoxArrowUpRight />
+        </button>
+         <button className="gthb">
+          <FaGithub className="cat"/> GitHub
+        </button>
+    </div>
         </div>
     </div>
 </>
